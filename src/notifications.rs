c246ca0b3ec8b0218fc3c66494f698857b972c87
Your Name <you@example.com>
2024-04-09 21:42:08 +0200

    fine-grain notifications (table level)

--- src/notifications.rs
+++ src/notifications.rs
@@ -1,5 +1,7 @@
 #![allow(non_camel_case_types)]
 use dashmap::DashMap;
+use std::collections::HashMap;
+
 use flume::{Receiver, Sender};
 use libsqlite3_sys as ffi;
 use std::sync::Arc;
@@ -16,6 +18,10 @@ pub struct NotificationDispatcher {
     pub(crate) tx: Sender<Notification>,
     //pub rx: Receiver<Notification>,
     pub clients: Arc<DashMap<ClientId, Sender<Notification>>>,
+    pub table_clients: Arc<DashMap<
+      String,
+      HashMap<ClientId, Sender<Notification>>
+    >>,
 }
 
 impl NotificationDispatcher {
@@ -25,8 +31,20 @@ impl NotificationDispatcher {
         self.clients.insert(client_id, tx);
         (client_id, rx)
     }
+    pub fn register_client_for_table(&self,table_name:& str) -> (ClientId, Receiver<Notification>) {
+        let client_id: ClientId = self.clients.len() as _;
+        let (tx, rx) = flume::unbounded();
+        let mut entry=self.table_clients.entry(table_name.into()).or_insert(Default::default());
+        entry.insert(client_id, tx);
+        (client_id, rx)
+    }
     pub fn unregister_client(&self, client_id: ClientId) {
         self.clients.remove(&client_id);
+    }
+    pub fn unregister_client_for_table(&self, client_id: ClientId,table_name: &String) {
+        if let Some(mut clients) = self.table_clients.get_mut(table_name) {
+          clients.value_mut().remove(&client_id);
+        }
     }
 }
 
@@ -35,6 +53,10 @@ impl Default for NotificationDispatcher {
         let (tx, rx) = flume::unbounded::<Notification>();
         let clients: Arc<DashMap<ClientId, Sender<Notification>>> = Default::default();
         let clients2 = clients.clone();
+        
+        let table_clients: Arc<DashMap<String,HashMap<ClientId, Sender<Notification>>>> = Default::default();
+        let table_clients2 = table_clients.clone();
+        
         let _ = std::thread::spawn(move || loop {
             match rx.recv() {
                 Ok(msg) => {
@@ -42,6 +64,15 @@ impl Default for NotificationDispatcher {
                     for client in clients.iter() {
                         tracing::debug!("sending notification: {:?}", msg);
                         let _ = client.value().send(msg.clone());
+                    }
+                    if let Some(table)=msg.table() {
+                      if let Some(clients) = table_clients.get(table) {
+                        let clients = clients.value();
+                        for (_,client) in clients.iter() {
+                          tracing::debug!("sending notification: {:?}", msg);
+                          let _ = client.send(msg.clone());
+                        }
+                      }
                     }
                 }
                 Err(err) => {
@@ -53,6 +84,7 @@ impl Default for NotificationDispatcher {
         Self {
             tx,
             clients: clients2,
+            table_clients: table_clients2,
         }
     }
 }
